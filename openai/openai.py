0f2f09a94580423242274815d4ada5ba7bff8e3e
kfriday32 <kfriday32@gmail.com>
2023-03-31 20:04:59 -0400

    intiailize openai

---
+++ openai/openai.py
@@ -0,0 +1,34 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPEN_AI_KEY");
+
+# get user input and send request to open ai 
+def generate_prompt():
+
+    # get user input
+    interest = input("Primary interest: ")
+    # generate prompt to send to api
+    prompt = f"Find all events that have titles similarly related to {interest}"
+
+    # create request
+    try:
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+    except Exception as error:
+        print("OpenAI Completion error!: ", error)
+
+
+
+def main():
+    generate_prompt()
+
+if __name__ == '__main__':
+    main()
