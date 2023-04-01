6d1bb5a3ab1d7bf7229527cc8370f53936415b65
kfriday32 <kfriday32@gmail.com>
2023-04-01 14:15:39 -0400

    publish event from server in progress

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from gpt import generate_prompt
+import mongodb
 import json
 import re
 
 app = Flask(__name__)
 
-
+# route to get events based on interests
 @app.route("/", methods=("GET", "POST"))
 def call_gpt():
     if request.method == "POST":
@@ -31,6 +32,26 @@ def call_gpt():
         DEBUG("GET request recieved")
 
     return "success!"
+
+
+# route to publish events
+@app.route('/publish', methods=["GET", "POST"])
+def publish_event():
+    if request.method == "GET":
+        return "<p>publish get request</p>"
+    else:
+        # get data to publish from post request
+        data = request.get_json()
+        print(data)
+
+        # post event to MongoDB
+        mongodb.publish_event()
+
+        title = data['title']
+        print(title)
+
+        # send post request to mongodb
+        return "<p>publish post request</p>"
 
 
 def DEBUG(message):
--- server/mongodb.py
+++ server/mongodb.py
@@ -4,26 +4,37 @@ import os
 from pymongo import MongoClient
 
 load_dotenv()
+MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 
-def get_mongodb():
+def connect_to_db():
     # connect to client (mongo user password should be stored in personal .env file)
-    mongo_client = MongoClient(f'mongodb+srv://cpreciad:{MONGO_PASSWORD}@test-cluster1.ljrkvvp.mongodb.net/?retryWrites=true&w=majority')
+    try:
+        uri = f'mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@test-cluster1.ljrkvvp.mongodb.net/?retryWrites=true&w=majority'
+        print(uri)
+        mongo_client = MongoClient(uri)
+    except Exception as e:
+        print(e)
+
+# retrieve data from 'event_list' collection
+def get_collection_data():
+
+    mongo_client = connect_to_db()
 
     # access 'campus_events' database
     db = mongo_client['campus_events']
-
     # access 'event_list' collection
     collection = db['event_list']
-
     data = collection.find()
 
     # return a list of dictionaries from the database
     return [document for document in data]
     
+
+# return subset of event data
 def get_partial_data():
     # called from openai.py library for the prompt construction
-    data = get_mongodb()
+    data = get_collection_data()
 
     # format the data to only include the titles and descriptions
 
@@ -35,9 +46,22 @@ def get_partial_data():
     return partial_data
 
 
+# publish data to collection
+def publish_event():
+    mongo_client = connect_to_db()
+    # Send a ping to confirm a successful connection
+    try:
+        print(mongo_client)
+        mongo_client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+
+
 def main():
-    get_mongodb()
+    get_collection_data()
     print(get_partial_data())
+    publish_event()
 
 
 if __name__ == '__main__':
