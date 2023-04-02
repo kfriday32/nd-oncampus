fc9de0e9884b21aa15adc9ee9e8ea842bde2efa1
Gavin Uhran <gavinu2001@gmail.com>
2023-04-02 08:36:11 -0400

    Changes

--- flutter_ui/lib/src/events_list.dart
+++ flutter_ui/lib/src/events_list.dart
@@ -21,35 +21,33 @@ class EventsList extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 10.0, right: 10.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 20),
               EventsListSection(
-                title: 'Today',
-                events: eventDataToday,
-                displayTime: true,
-                refreshFollowing: refreshFollowing
-              ),
+                  title: 'Today',
+                  events: eventDataToday,
+                  displayTime: true,
+                  refreshFollowing: refreshFollowing),
               const SizedBox(
                 height: 20,
               ),
               EventsListSection(
-                title: 'This Week',
-                events: eventDataThisWeek,
-                displayTime: false,
-                refreshFollowing: refreshFollowing
-              ),
+                  title: 'This Week',
+                  events: eventDataThisWeek,
+                  displayTime: false,
+                  refreshFollowing: refreshFollowing),
               const SizedBox(
                 height: 20,
               ),
               EventsListSection(
-                title: 'Upcoming',
-                events: eventDataUpcoming,
-                displayTime: false,
-                refreshFollowing: refreshFollowing
-              ),
+                  title: 'Upcoming',
+                  events: eventDataUpcoming,
+                  displayTime: false,
+                  refreshFollowing: refreshFollowing),
               const SizedBox(height: 10),
             ],
           ),
@@ -85,186 +83,195 @@ class EventsListSection extends StatelessWidget {
             fontSize: 28,
           ),
         ),
-        ListView.builder(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: events.length,
-          itemBuilder: (BuildContext context, int index) {
-            final event = events[index];
-            final String eventTitle = event['title']!;
-            final String eventLocation = event['location']!;
-            final String eventTime =
-                DateFormat('h:mm a').format(event['startTime']!);
-            final String eventMonth =
-                DateFormat('MMM').format(event['startTime']);
-            final String eventDay = event['startTime'].day.toString();
+        (events.isEmpty)
+            ? const Padding(
+                padding: EdgeInsets.only(top: 5.0),
+                child: Text('No events to display'),
+              )
+            : ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: events.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final event = events[index];
+                  final String eventTitle = event['title']!;
+                  final String eventLocation = event['location']!;
+                  final String eventTime =
+                      DateFormat('h:mm a').format(event['startTime']!);
+                  final String eventMonth =
+                      DateFormat('MMM').format(event['startTime']);
+                  final String eventDay = event['startTime'].day.toString();
 
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => EventDetailPage(event: events[index], refreshFollowing: refreshFollowing),
-                  ),
-                );
-              },
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 5.0,
-                ),
-                child: Container(
-                  height: 100.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(7.5),
-                    border: Border(
-                      top: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EventDetailPage(
+                              event: events[index],
+                              refreshFollowing: refreshFollowing),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5.0,
                       ),
-                      bottom: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                      left: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                      right: BorderSide(
-                        color: Colors.grey[300]!,
-                        width: 1,
-                      ),
-                    ),
-                  ),
-                  child: Expanded(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 80,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200]!,
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(7.5),
-                              bottomLeft: Radius.circular(7.5),
+                      child: Container(
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7.5),
+                          border: Border(
+                            top: BorderSide(
+                              color: Colors.grey[300]!,
+                              width: 1,
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.grey[300]!,
+                              width: 1,
+                            ),
+                            left: BorderSide(
+                              color: Colors.grey[300]!,
+                              width: 1,
+                            ),
+                            right: BorderSide(
+                              color: Colors.grey[300]!,
+                              width: 1,
                             ),
                           ),
-                          child: displayTime
-                              ? Center(
-                                  child: Text(
-                                    eventTime,
-                                    style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 22.0,
-                                      height: 1.25,
-                                    ),
-                                    textAlign: TextAlign.center,
+                        ),
+                        child: Expanded(
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey[200]!,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(7.5),
+                                    bottomLeft: Radius.circular(7.5),
                                   ),
-                                )
-                              : Center(
+                                ),
+                                child: displayTime
+                                    ? Center(
+                                        child: Text(
+                                          eventTime,
+                                          style: const TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 22.0,
+                                            height: 1.25,
+                                          ),
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      )
+                                    : Center(
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Text(
+                                              eventDay,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 32.0,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 2.5),
+                                            Text(
+                                              eventMonth,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 14.0,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                              ),
+                              const SizedBox(
+                                width: 5.0,
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10.0,
+                                    vertical: 10.0,
+                                  ),
                                   child: Column(
                                     mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
                                       Text(
-                                        eventDay,
+                                        eventTitle,
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
                                         style: const TextStyle(
                                           fontWeight: FontWeight.bold,
-                                          fontSize: 32.0,
+                                          fontSize: 18,
                                         ),
+                                      ),
+                                      const SizedBox(
+                                        height: 2.5,
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text(
+                                            'Hosted by ',
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.normal,
+                                              fontSize: 12,
+                                              color: Colors.grey[500]!,
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                              event['host'],
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              style: const TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 12,
+                                                color: Colors.blue,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
                                       ),
                                       const SizedBox(height: 2.5),
-                                      Text(
-                                        eventMonth,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 14.0,
-                                        ),
-                                      ),
+                                      Row(
+                                        children: [
+                                          Icon(
+                                            Icons.location_on_outlined,
+                                            size: 14,
+                                            color: Colors.grey[500]!,
+                                          ),
+                                          const SizedBox(width: 2.5),
+                                          Expanded(
+                                            child: Text(
+                                              eventLocation,
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              style: TextStyle(
+                                                color: Colors.grey[500]!,
+                                                fontSize: 12,
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      )
                                     ],
                                   ),
                                 ),
-                        ),
-                        const SizedBox(
-                          width: 5.0,
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10.0,
-                              vertical: 10.0,
-                            ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  eventTitle,
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 2,
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 2.5,
-                                ),
-                                Row(
-                                  children: [
-                                    Text(
-                                      'Hosted by ',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.normal,
-                                        fontSize: 12,
-                                        color: Colors.grey[500]!,
-                                      ),
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        event['host'],
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 12,
-                                          color: Colors.blue,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                const SizedBox(height: 2.5),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.location_on_outlined,
-                                      size: 14,
-                                      color: Colors.grey[500]!,
-                                    ),
-                                    const SizedBox(width: 2.5),
-                                    Expanded(
-                                      child: Text(
-                                        eventLocation,
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 1,
-                                        style: TextStyle(
-                                          color: Colors.grey[500]!,
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
       ],
     );
   }
