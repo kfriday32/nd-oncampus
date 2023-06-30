aab94646f3db616faa7a39af4d482c2fc44c434c
claireyyin <cyin5@nd.edu>
2023-06-30 15:54:26 +0800

    event series page done

--- flutter_ui/lib/src/home_page.dart
+++ flutter_ui/lib/src/home_page.dart
@@ -50,8 +50,8 @@ class _HomePageState extends State<HomePage>
     // Initialize the controller with the number of tabs and this class as the provider
     _tabController = TabController(length: myTabs.length, vsync: this);
     _loadAllEvents();
-    // _loadSuggestedEvents();
-    // _loadFollowingEvents();
+    _loadSuggestedEvents();
+    _loadFollowingEvents();
   }
 
   @override
